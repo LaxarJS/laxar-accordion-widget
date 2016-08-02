2542c8f7422222dd5be833cd95284dd9d9011800
Michael Kurze <michael.kurze@aixigo.de>
2016-08-02 17:30:19 +0200

    use new axVisibility injection (laxar#333)

--- ax-accordion-widget.js
+++ ax-accordion-widget.js
@@ -9,13 +9,13 @@ import * as ax from 'laxar';
 import * as patterns from 'laxar-patterns';
 import 'laxar-accordion-control/ax-accordion-control';
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Controller.$inject = [ '$scope', '$q' ];
+Controller.$inject = [ '$scope', 'axEventBus', 'axFeatures', 'axLog', 'axVisibility' ];
 
-function Controller( $scope, $q ) {
+function Controller( $scope, eventBus, features, log, visibility ) {
 
-   let requestedPanel = -1;
+   let requestedPanelIndex = -1;
    let allowNextPanelActivation = false;
 
    $scope.model = {
@@ -26,14 +26,14 @@ function Controller( $scope, $q ) {
    const localize = patterns.i18n.handlerFor( $scope ).scopeLocaleFromFeature( 'i18n', {
       onChange() {
          $scope.model.panels.forEach( ( areaModel, index ) => {
-            areaModel.htmlLabel = localize( $scope.features.areas[ index ].i18nHtmlLabel );
+            areaModel.htmlLabel = localize( features.areas[ index ].i18nHtmlLabel );
          } );
       }
    } ).localizer();
 
-   $scope.model.panels = $scope.features.areas.map( createPanelModel );
+   $scope.model.panels = features.areas.map( createPanelModel );
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    $scope.eventBus.subscribe( 'beginLifecycleRequest', () => {
       $scope.model.panels.forEach( ( panel, i ) => {
@@ -41,34 +41,22 @@ function Controller( $scope, $q ) {
       } );
    } );
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   let initialVisibilityPublished = false;
-   patterns.visibility.handlerFor( $scope, {
-      // set nested area visibility depending on the `areaShowing` state and on the context visibility
-      onAnyAreaRequest( event ) {
-         const eventArea = event.area.substring( $scope.widget.id.length + 1 );
-         const index = isOk( $scope.model.selectedPanel ) ? $scope.model.selectedPanel : requestedPanel;
-         const selectedArea = $scope.model.panels[ index ].areaName;
-         return event.visible && eventArea === selectedArea;
+   visibility.updateAreaVisibility( visibilityByArea() );
 
-         //////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-         function isOk( index ) {
-            return ng.isNumber( index ) && index >= 0;
-         }
-      },
-      onShow() {
-         if( !initialVisibilityPublished ) {
-            const localName = $scope.model.panels[ $scope.model.selectedPanel ].areaName;
-            const areaName = `${$scope.widget.id}.${localName}`;
-            patterns.visibility.requestPublisherForArea( $scope, areaName )( true );
-            initialVisibilityPublished = true;
-         }
-      }
-   } );
+   function visibilityByArea() {
+      const result = {};
+      const { selectedPanel } = $scope.model;
+      $scope.model.panels.forEach( ( panel, i ) => {
+         result[ panel.areaName ] = i === selectedPanel;
+      } );
+      return result;
+   }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    function createPanelModel( area, index ) {
 
@@ -110,57 +98,41 @@ function Controller( $scope, $q ) {
       return areaModel;
    }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   // Make the given panel visible, make sure that contents are loaded (visible) first.
    function setSelectedPanel( newIndex ) {
-      console.log( "setSelectedPanel", newIndex );
-      const oldIndex = $scope.model.selectedPanel;
-
-      if( !isNaN( oldIndex ) ) {
-         $scope.model.panels[ oldIndex ].classes.active = false;
-         publishFlagIfConfigured( oldIndex, false );
-         publishVisibility( oldIndex, false );
+      const visibilityChanges = {};
+      const previousIndex = $scope.model.selectedPanel;
+      if( !isNaN( previousIndex ) ) {
+         const previousPanel = $scope.model.panels[ previousIndex ];
+         previousPanel.classes.active = false;
+         publishFlagIfConfigured( previousIndex, false );
+         visibilityChanges[ previousPanel.areaName ] = false;
       }
 
       if( !isNaN( newIndex ) ) {
-         requestedPanel = newIndex;
+         requestedPanelIndex = newIndex;
+         const requestedPanel = $scope.model.panels[ newIndex ];
          publishFlagIfConfigured( newIndex, true );
-         publishVisibility( newIndex, true )
-            .then( () => {
-               implementSelection( newIndex );
-            } );
+         const action = requestedPanel.selection || null;
+         if( action ) {
+            $scope.eventBus.publish( `takeActionRequest.${action}`, { action } );
+         }
+         visibilityChanges[ requestedPanel.areaName ] = true;
       }
 
-      const selectionAction = $scope.features.areas[ newIndex ].selection &&
-                            $scope.features.areas[ newIndex ].selection.action;
-      if( selectionAction ) {
-         $scope.eventBus.publish( `takeActionRequest.${selectionAction}`, {
-            action: selectionAction
+      if( !isNaN( previousIndex ) || !isNaN( newIndex ) ) {
+         visibility.updateAreaVisibility( visibilityChanges ).then( () => {
+            if( isNaN( requestedPanelIndex ) ) { return; }
+            const requestedPanel = $scope.model.panels[ requestedPanelIndex ];
+            requestedPanel.classes.active = true;
+            $scope.model.selectedPanel = newIndex;
+            allowNextPanelActivation = true;
          } );
       }
-
-      /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-      function publishVisibility( index, visibility ) {
-         if( !$scope.isVisible ) {
-            return $q.when();
-         }
-         const areaName = [ $scope.widget.id, $scope.model.panels[ index ].areaName ].join( '.' );
-         return patterns.visibility.requestPublisherForArea( $scope, areaName )( visibility );
-      }
-
-      /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-      function implementSelection( index ) {
-         $scope.model.panels[ index ].classes.active = true;
-         $scope.model.selectedPanel = newIndex;
-         allowNextPanelActivation = true;
-      }
-
    }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    function createSelectionRequestTriggerHandler( areaFeatures, index ) {
       return function() {
@@ -173,46 +145,35 @@ function Controller( $scope, $q ) {
       };
    }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    function handleDidConfirmSelection() {
-      if( requestedPanel < 0 ) {
-         ax.log.debug( 'Received selection confirmation, but no panel selection was requested.' );
+      if( requestedPanelIndex < 0 ) {
+         log.debug( 'Received selection confirmation, but no panel selection was requested.' );
          return;
       }
-      if( requestedPanel > $scope.model.panels.length - 1 ) {
-         ax.log.warn( 'Received selection confirmation, but pending index [0] exceeds maximum of [1].',
-                   requestedPanel,
-                   $scope.model.panels.length - 1 );
-         requestedPanel = -1;
-         return;
-      }
-
-      setSelectedPanel( requestedPanel );
+      setSelectedPanel( requestedPanelIndex );
    }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    $scope.model.onBeforeActivate = function( index ) {
       if( $scope.model.panels[ index ].classes.disabled ) {
          return false;
       }
 
-      if( allowNextPanelActivation && index === requestedPanel ) {
-         requestedPanel = -1;
+      if( allowNextPanelActivation && index === requestedPanelIndex ) {
+         requestedPanelIndex = -1;
          allowNextPanelActivation = false;
          // selection was already confirmed, allow UI to reflect it now:
          return true;
       }
 
-      const areaConfig = $scope.features.areas[ index ];
-      const selectionRequestAction = areaConfig.selectionRequest &&
-                                   areaConfig.selectionRequest.action;
-      if( selectionRequestAction ) {
-         requestedPanel = index;
-         $scope.eventBus.publish( `takeActionRequest.${selectionRequestAction}`, {
-            action: selectionRequestAction
-         } );
+      const { selectionRequest } = features.areas[ index ] || {};
+      if( selectionRequest ) {
+         const { action } = selectionRequest || {};
+         requestedPanelIndex = index;
+         $scope.eventBus.publish( `takeActionRequest.${action}`, { action } );
       }
       else {
          setSelectedPanel( index );
@@ -222,12 +183,10 @@ function Controller( $scope, $q ) {
       return false;
    };
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    function configureFlagHandler( flag, handler ) {
-      if( !flag ) {
-         return;
-      }
+      if( !flag ) { return; }
       const flagName = flag.replace( /^!/, '' );
       const isInverted = flagName !== flag;
       $scope.eventBus.subscribe( `didChangeFlag.${flagName}`, event => {
@@ -235,21 +194,18 @@ function Controller( $scope, $q ) {
       } );
    }
 
-   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    function publishFlagIfConfigured( areaIndex, state ) {
-      const flag = $scope.features.areas[ areaIndex ].flag;
+      const { flag } = features.areas[ areaIndex ];
       if( flag ) {
-         $scope.eventBus.publish( `didChangeFlag.${flag}.${state}`, {
-            flag,
-            state
-         } );
+         $scope.eventBus.publish( `didChangeFlag.${flag}.${state}`, { flag, state } );
       }
    }
 
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export const name = ng.module( 'axAccordionWidget', [] )
    .controller( 'AxAccordionWidgetController', Controller ).name;
